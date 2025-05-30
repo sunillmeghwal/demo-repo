034b4859c4f068497bfdeb84a8b877b723bc9ba4
sunillmeghwal <sunillmeghwal@gmail.com>
2025-05-30 19:25:26 +0530

    my college

--- context-api/src/College.jsx
+++ context-api/src/College.jsx
@@ -8,6 +8,7 @@ export default function College() {
   return (
  <div style={{backgroundColor:'orange',padding:'10px'}}>
   <h1> College Component</h1>
+  <h1>my college</h1>
   <ClassComponent/>
  </div>
   )
